1634678a127b71b0aac9ab5e2d03d2b0ddc3436d
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-04-16 18:49:47 +0200

    Initial Commit

---
+++ src/main/java/com/enterpriseintellijence/enterpriseintellijence/EnterpriseIntellijenceApplication.java
@@ -0,0 +1,13 @@
+package com.enterpriseintellijence.enterpriseintellijence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseIntellijenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseIntellijenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enterpriseintellijence/enterpriseintellijence/EnterpriseIntellijenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterpriseintellijence.enterpriseintellijence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseIntellijenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
