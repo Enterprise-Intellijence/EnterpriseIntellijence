67a63998af753284da70268ce664882e4f304cf6
Ernesto-Rapisarda <ernesto.rapisarda81@gmail.com>
2023-04-21 10:34:39 +0200

    added basic repository

---
+++ src/main/java/com/enterpriseintellijence/enterpriseintellijence/data/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.enterpriseintellijence.enterpriseintellijence.data.repository;
+
+import com.enterpriseintellijence.enterpriseintellijence.data.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,String> {
+}
