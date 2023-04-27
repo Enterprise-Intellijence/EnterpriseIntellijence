886d3060753ddd18b6d77f88bd7890ed8805cc17
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-04-27 20:38:22 +0200

    implementing  JWT utils

---
+++ src/main/java/com/enterpriseintellijence/enterpriseintellijence/security/JwtContextUtils.java
@@ -0,0 +1,27 @@
+package com.enterpriseintellijence.enterpriseintellijence.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public class JwtContextUtils {
+
+    public static Optional<String> getUsernameFromContext() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        if (authentication == null) {
+            return Optional.empty();
+        }
+
+        if(authentication.getPrincipal() instanceof UserDetails userDetails) {
+            return Optional.of(userDetails.getUsername());
+        } else if(authentication.getPrincipal() instanceof CustomOAuth2User customOAuth2User) {
+            return Optional.of(customOAuth2User.getUsername());
+        }
+
+        return Optional.empty();
+    }
+}
