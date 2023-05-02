cba665c7ea7bbe81641f8c621925a2970200bfb3
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2023-05-02 14:07:27 +0200

    Added some validation to PaymentMethodDTO

--- src/main/java/com/enterpriseintellijence/enterpriseintellijence/dto/PaymentMethodDTO.java
+++ src/main/java/com/enterpriseintellijence/enterpriseintellijence/dto/PaymentMethodDTO.java
@@ -1,9 +1,13 @@
 package com.enterpriseintellijence.enterpriseintellijence.dto;
 
 import com.enterpriseintellijence.enterpriseintellijence.data.entities.User;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor
@@ -12,12 +16,20 @@ public class PaymentMethodDTO {
 
     private String id;
 
-    // TODO: usare pattern per la carta di credito
-
+    @NotBlank
+    @Length(min = 12, max = 12)
     private String creditCard;
+
+    @NotBlank
+    @Length(min = 5, max = 5)
     private String expiryDate;
+
+    @NotBlank
+    @Length(max = 25)
     private String owner;
+
     private UserDTO defaultUser;
+
     private UserDTO ownerUser;
 
 }
