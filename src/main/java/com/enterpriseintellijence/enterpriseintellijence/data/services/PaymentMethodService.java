0bb22fdb9cf4fd90485fe07e4adc8649be937a0c
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2023-05-02 14:11:17 +0200

    Now PaymentMethodService methods returns a DTO

--- src/main/java/com/enterpriseintellijence/enterpriseintellijence/data/services/PaymentMethodService.java
+++ src/main/java/com/enterpriseintellijence/enterpriseintellijence/data/services/PaymentMethodService.java
@@ -6,9 +6,8 @@ import org.springframework.http.ResponseEntity;
 public interface PaymentMethodService {
 
         PaymentMethodDTO createPaymentMethod(PaymentMethodDTO paymentMethodDTO);
-        ResponseEntity<PaymentMethodDTO> replacePaymentMethod(String id, PaymentMethodDTO paymentMethodDTO);
-        ResponseEntity<PaymentMethodDTO> updatePaymentMethod(String id, PaymentMethodDTO paymentMethodDTO);
-        ResponseEntity<PaymentMethodDTO> deletePaymentMethod(String id);
-        ResponseEntity<PaymentMethodDTO> getPaymentMethod(String id);
-
+        PaymentMethodDTO replacePaymentMethod(String id, PaymentMethodDTO paymentMethodDTO);
+        PaymentMethodDTO updatePaymentMethod(String id, PaymentMethodDTO paymentMethodDTO);
+        PaymentMethodDTO deletePaymentMethod(String id);
+        PaymentMethodDTO getPaymentMethod(String id);
 }
