593b01d207863fc774e82f1297446fe1277e1042
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2023-05-04 17:36:35 +0200

    Added interface to PagingAndSortingRepository

--- src/main/java/com/enterpriseintellijence/enterpriseintellijence/data/repository/PaymentMethodRepository.java
+++ src/main/java/com/enterpriseintellijence/enterpriseintellijence/data/repository/PaymentMethodRepository.java
@@ -1,8 +1,13 @@
 package com.enterpriseintellijence.enterpriseintellijence.data.repository;
 
 import com.enterpriseintellijence.enterpriseintellijence.data.entities.PaymentMethod;
+import com.enterpriseintellijence.enterpriseintellijence.dto.PaymentMethodDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, String>, JpaSpecificationExecutor<PaymentMethod> {
+public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, String>, JpaSpecificationExecutor<PaymentMethod>, PagingAndSortingRepository<PaymentMethod, String> {
+    Page<PaymentMethodDTO> findAllByDefaultUser_Id(String id, Pageable pageable);
 }
