a64076d23a6c84333dc29961c4a35d7cef1c2364
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-05-07 19:04:57 +0200

    removed extra bean config

--- src/main/java/com/enterpriseintellijence/enterpriseintellijence/config/BeanConfig.java
+++
@@ -1,9 +0,0 @@
-package com.enterpriseintellijence.enterpriseintellijence.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = { "com.enterpriseintellijence.enterpriseintellijence" })
-public class BeanConfig {
-}
