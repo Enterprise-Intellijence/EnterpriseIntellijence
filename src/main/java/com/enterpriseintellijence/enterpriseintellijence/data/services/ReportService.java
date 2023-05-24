2244e3ca6f2d976e0ebddc51257180ff84f0f2fc
Francesco Gallo <francescogallo728@gmail.com>
2023-05-24 15:45:01 +0200

    Report full stack + filterchain

--- src/main/java/com/enterpriseintellijence/enterpriseintellijence/data/services/ReportService.java
+++ src/main/java/com/enterpriseintellijence/enterpriseintellijence/data/services/ReportService.java
@@ -1,5 +1,14 @@
 package com.enterpriseintellijence.enterpriseintellijence.data.services;
 
+import com.enterpriseintellijence.enterpriseintellijence.dto.ReportDTO;
+import com.enterpriseintellijence.enterpriseintellijence.dto.enums.ReportStatus;
+import org.springframework.data.domain.Page;
+
 public interface ReportService {
 
+    ReportDTO createReport(ReportDTO reportDTO);
+
+    ReportDTO closeReport(String reportId);
+
+    Page<ReportDTO> getReportsByStatus(ReportStatus status, int page, int size);
 }
