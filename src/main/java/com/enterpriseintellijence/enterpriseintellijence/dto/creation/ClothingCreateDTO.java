d6928e3a59c2d78b2845f67b6eb04f6800a06d70
Ernesto-Rapisarda <ernesto.rapisarda81@gmail.com>
2023-06-21 22:02:06 +0200

    fixed creation product

---
+++ src/main/java/com/enterpriseintellijence/enterpriseintellijence/dto/creation/ClothingCreateDTO.java
@@ -0,0 +1,21 @@
+package com.enterpriseintellijence.enterpriseintellijence.dto.creation;
+
+import com.enterpriseintellijence.enterpriseintellijence.dto.SizeDTO;
+import com.enterpriseintellijence.enterpriseintellijence.dto.enums.Colour;
+import com.enterpriseintellijence.enterpriseintellijence.dto.enums.ProductGender;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@ToString
+@Builder
+@AllArgsConstructor
+public class ClothingCreateDTO {
+    @NotNull
+    private ProductGender productGender;
+    @NotNull
+    private SizeDTO clothingSize;
+    @NotNull
+    private Colour colour;
+}
