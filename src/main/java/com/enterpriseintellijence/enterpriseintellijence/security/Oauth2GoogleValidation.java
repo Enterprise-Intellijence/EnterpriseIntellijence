f319657b8b4300b5257f26ef91772d12fc914c00
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-06-28 01:57:00 +0200

    .

--- src/main/java/com/enterpriseintellijence/enterpriseintellijence/security/Oauth2GoogleValidation.java
+++ src/main/java/com/enterpriseintellijence/enterpriseintellijence/security/Oauth2GoogleValidation.java
@@ -25,18 +25,21 @@ import java.util.Properties;
 public class Oauth2GoogleValidation {
 
 
-//    @Value("${spring.security.oauth2.client.registration.google.clientId}")
-    private String clientId = "1049827257011-ete5d6u7kjq601i6ra2ukaus5nsd3het.apps.googleusercontent.com";
+    @Value("${spring.security.oauth2.client.registration.google.clientId}")
+    private String clientId;
 
 
 
-    private final GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), GsonFactory.getDefaultInstance())
+    private GoogleIdTokenVerifier verifier;
+
+    public Map<String, String> validate(String idTokenString) throws Exception {
+        if(verifier == null) {
+            verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), GsonFactory.getDefaultInstance())
                 .setAudience(Collections.singleton(clientId))
                 .setIssuer("https://accounts.google.com")
                 .setClock(Clock.SYSTEM)
                 .build();
-
-    public Map<String, String> validate(String idTokenString) throws Exception {
+        }
 
         GoogleIdToken idToken = verifier.verify(idTokenString);
 
@@ -51,7 +54,6 @@ public class Oauth2GoogleValidation {
             boolean emailVerified = payload.getEmailVerified();
             String name = (String) payload.get("name");
             String pictureUrl = (String) payload.get("picture");
-            String locale = (String) payload.get("locale");
             String familyName = (String) payload.get("family_name");
             String givenName = (String) payload.get("given_name");
 
@@ -59,7 +61,6 @@ public class Oauth2GoogleValidation {
             System.out.println("Email verified: " + emailVerified);
             System.out.println("Name: " + name);
             System.out.println("Picture URL: " + pictureUrl);
-            System.out.println("Locale: " + locale);
             System.out.println("Family Name: " + familyName);
             System.out.println("Given Name: " + givenName);
 
