ccb19641d7c35ff11f00459f98010445fb31faba
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-07-01 12:41:36 +0200

    implemented services and controllers for conversations

--- src/main/java/com/enterpriseintellijence/enterpriseintellijence/core/services/NotificationSystem.java
+++ src/main/java/com/enterpriseintellijence/enterpriseintellijence/core/services/NotificationSystem.java
@@ -3,18 +3,17 @@ package com.enterpriseintellijence.enterpriseintellijence.core.services;
 import com.enterpriseintellijence.enterpriseintellijence.data.entities.Message;
 import com.enterpriseintellijence.enterpriseintellijence.data.entities.Offer;
 import com.enterpriseintellijence.enterpriseintellijence.data.entities.Product;
-import com.enterpriseintellijence.enterpriseintellijence.data.entities.User;
 import com.enterpriseintellijence.enterpriseintellijence.data.repository.MessageRepository;
 import com.enterpriseintellijence.enterpriseintellijence.data.services.MessageService;
+import com.enterpriseintellijence.enterpriseintellijence.dto.ConversationDTO;
 import com.enterpriseintellijence.enterpriseintellijence.dto.enums.MessageStatus;
 import com.enterpriseintellijence.enterpriseintellijence.security.JwtContextUtils;
-import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -26,28 +25,30 @@ public class NotificationSystem {
     private final JwtContextUtils jwtContextUtils;
     private final Clock clock;
 
-    public Message offerCreatedNotification(Offer offer){
+    public Message offerCreatedNotification(Offer offer) {
 
         Product product = offer.getProduct();
 
 
-        String convID = messageService.getConversationId(offer.getOfferer(), product.getSeller(), product);
+        Optional<ConversationDTO> conversationOptional = messageService.getConversation(offer.getOfferer(), product.getSeller(), product);
+
+        String conversationId = conversationOptional.isPresent() ? conversationOptional.get().getConversationId() : UUID.randomUUID().toString();
 
 
         return Message.builder()
-                .text("Hi, here is my offer for " + product.getTitle() + ".")
-                .messageDate(LocalDateTime.now(clock))
-                .messageStatus(MessageStatus.UNREAD)
-                .product(product)
-                .conversationId(convID)
-                .sendUser(offer.getOfferer())
-                .receivedUser(product.getSeller())
-                .offer(offer)
-                .build();
+            .text("Hi, here is my offer for " + product.getTitle() + ".")
+            .messageDate(LocalDateTime.now(clock))
+            .messageStatus(MessageStatus.UNREAD)
+            .product(product)
+            .conversationId(conversationId)
+            .sendUser(offer.getOfferer())
+            .receivedUser(product.getSeller())
+            .offer(offer)
+            .build();
     }
 
     public Message offerAcceptedOrRejectedNotification(Offer offer, boolean isAccepted) {
-        String basic = "Your offer for product " +offer.getProduct().getTitle();
+        String basic = "Your offer for product " + offer.getProduct().getTitle();
         if (isAccepted)
             basic = basic + " is accepted. Please check your profile and complete the order";
         else
@@ -55,15 +56,15 @@ public class NotificationSystem {
 
 
         return Message.builder()
-                .text(basic)
-                .messageDate(LocalDateTime.now(clock))
-                .messageStatus(MessageStatus.UNREAD)
-                .conversationId(offer.getMessage().getConversationId())
-                .product(offer.getProduct())
-                .sendUser(offer.getOfferer())
-                .receivedUser(offer.getProduct().getSeller())
-                .offer(offer)
-                .build();
+            .text(basic)
+            .messageDate(LocalDateTime.now(clock))
+            .messageStatus(MessageStatus.UNREAD)
+            .conversationId(offer.getMessage().getConversationId())
+            .product(offer.getProduct())
+            .sendUser(offer.getOfferer())
+            .receivedUser(offer.getProduct().getSeller())
+            .offer(offer)
+            .build();
 
     }
 
