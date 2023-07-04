ce26889ec0e493fbd812f7865543c0fb74746a21
Francesco Gallo <francescogallo728@gmail.com>
2023-07-04 20:49:42 +0200

    .

--- src/main/java/com/enterpriseintellijence/enterpriseintellijence/controller/DemoController.java
+++ src/main/java/com/enterpriseintellijence/enterpriseintellijence/controller/DemoController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +25,32 @@ public class DemoController {
 
     @GetMapping("/api/v1/ip")
     public String getIp() throws UnknownHostException {
-        return InetAddress.getLocalHost().getHostAddress();
+        return ip();
+    }
+
+    public String ip() {
+        try {
+            InetAddress localAddress = getLocalAddress();
+            return localAddress.getHostAddress();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private InetAddress getLocalAddress() throws SocketException {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = interfaces.nextElement();
+            Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+            while (addresses.hasMoreElements()) {
+                InetAddress address = addresses.nextElement();
+                if (!address.isLoopbackAddress() && !address.isLinkLocalAddress()
+                        && address.isSiteLocalAddress()) {
+                    return address;
+                }
+            }
+        }
+        throw new SocketException("Unable to determine local address");
     }
 }
